e2c33f39e4a8aba9917011da71ea9b9558403454
TilaposGr <tilemaxaki@gmail.com>
2026-06-17 17:34:37 +0100

    Add files via upload

--- apply-config.sh
+++ apply-config.sh
@@ -13,16 +13,17 @@ sudo dnf copr enable darkly -y
 sudo dnf install darkly -y
 kpackagetool6 --type Plasma/Style -i https://dl.opendesktop.org/api/files/download/id/1614339
 echo Instaled fastfetch and themes!
-kwriteconfig6 --file kdeglobals --group KDE --key widgetDtyle Darkly
+kwriteconfig6 --file kdeglobals --group KDE --key widgetStyle Darkly
 kwriteconfig6 --file plasmarc --group Theme --key name "Utterly-Round"
 ./icon.sh
 plasmashell --replace & disown
 echo Applied themes!
 kquitapp6 plasmashell && sleep 1
-sudo cp -f ~/Downloads/sensory-overload-os-main/plasmashellrc /home/sensory/.config/
+sudo cp -f ~/Downloads/sensory-overload-os-main/plasmashellrc ~/.config/
 plasmashell & disown
 echo Changed desktop layout!
-cp ~/Downloads/sensory-overload-os-main/Wallpapers ~/Pictures
+mkdir ~/Pictures/Wallpapers
+cp ~/Downloads/sensory-overload-os-main/Wallpapers/* ~/Pictures/Wallpapers
 kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group "Containments" --group "1" --group "Wallpaper" --group "org.kde.image" --group "General" --key "Image" "~/Pictures/Wallpapers/soswall.png"
 kquitapp6 plasmashell && sleep 1
 plasmashell & disown
