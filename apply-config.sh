b293c3e4d1899e8998021e92eab1acffe6c4b89c
TilaposGr <tilemaxaki@gmail.com>
2026-06-17 18:01:20 +0100

    Add files via upload

--- apply-config.sh
+++ apply-config.sh
@@ -4,27 +4,32 @@ sudo cp -f ~/Downloads/sensory-overload-os-main/os-release /etc/
 sudo cp -f ~/Downloads/sensory-overload-os-main/kcm-about-distrorc ~/.config/kdedefaults
 sudo cp -f ~/Downloads/sensory-overload-os-main/hostname /etc/
 echo Replaced OS info!
-mkdir -p ~/.config/fastfetch
-sudo cp ~/Downloads/sensory-overload-os-main/misc ~/Pictures
-sudo cp -f ~/Downloads/sensory-overload-os-main/config.jsonc ~/.config/fastfetch/
-echo Added custom fastfetch config!
 sudo dnf install fastfetch
-sudo dnf copr enable darkly -y
+sudo dnf copr enable deltacopy/darkly -y
 sudo dnf install darkly -y
-kpackagetool6 --type Plasma/Style -i https://dl.opendesktop.org/api/files/download/id/1614339
+mkdir -p ~/.local/share/plasma/desktoptheme/
+curl -L -o /tmp/utterly-round.tar.gz "https://dl.opendesktop.org/api/files/download/id/1614339"
+tar -xzf /tmp/utterly-round.tar.gz -C ~/.local/share/plasma/desktoptheme/ 2>/dev/null
+echo "Installed fastfetch and themes!"
 echo Instaled fastfetch and themes!
 kwriteconfig6 --file kdeglobals --group KDE --key widgetStyle Darkly
 kwriteconfig6 --file plasmarc --group Theme --key name "Utterly-Round"
+chmod +x icon.sh
 ./icon.sh
 plasmashell --replace & disown
 echo Applied themes!
+mkdir -p ~/.config/fastfetch
+fastfetch --gen-config-full
+sudo cp ~/Downloads/sensory-overload-os-main/misc ~/Pictures
+sudo cp -f ~/Downloads/sensory-overload-os-main/config.jsonc ~/.config/fastfetch/
+echo Added custom fastfetch config!
 kquitapp6 plasmashell && sleep 1
 sudo cp -f ~/Downloads/sensory-overload-os-main/plasmashellrc ~/.config/
 plasmashell & disown
 echo Changed desktop layout!
 mkdir ~/Pictures/Wallpapers
 cp ~/Downloads/sensory-overload-os-main/Wallpapers/* ~/Pictures/Wallpapers
-kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group "Containments" --group "1" --group "Wallpaper" --group "org.kde.image" --group "General" --key "Image" "~/Pictures/Wallpapers/soswall.png"
+plasma-apply-wallpaperimage ~/Pictures/Wallpapers/soswall.png
 kquitapp6 plasmashell && sleep 1
 plasmashell & disown
 echo Applied wallpaper!
